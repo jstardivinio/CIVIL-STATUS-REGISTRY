5e737d56760487f0903f9d43e3f2c45a6a4a7a5c
jstardivinio <35686757+jstardivinio@users.noreply.github.com>
2019-05-25 21:53:58 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home page</title>
+    <link href="../style/style.css" type="text/css" rel="stylesheet" />
+  </head>
+  <body>
+
+<header>
+<div class ="header">
+<nav>
+<button class="link" type="button" name="add"><a class="normal" href ="registerUser.php">Add user</a></button>
+
+<form action="logout.inc.php" method="post">
+  <button type="submit" name="logout">logout</button>
+</form>
+</nav>
+</div>
+</header>
+  </body>
+</html>
