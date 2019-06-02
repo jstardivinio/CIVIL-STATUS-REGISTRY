0d70c238db50c2bd860266133a48586812f2d5c1
jstardivinio <35686757+jstardivinio@users.noreply.github.com>
2019-06-02 15:31:05 +0100

    Add files via upload
    Here some identifiers have been changed  due to the updates of the fields names in the database.

--- conndb.php
+++ conndb.php
@@ -2,7 +2,7 @@
 $dbServer = "localhost";
 $dbUsername = "root";
 $dbPassword ="";
-$dbName = "WORKER";
+$dbName = "CIVIL_STATUS_REGISTERY";
 
-$conn = mysqli_connect($dbServer, $dbUsername, $dbPassword) or die("Could not connect to database: ". mysqli_error());
+$conn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName) or die("Could not connect to database: ". mysqli_error());
 ?>
